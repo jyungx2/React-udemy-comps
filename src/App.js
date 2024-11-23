a98fd08ebbda219b862abc520c87fbe6bdb3ecc4
jiyoungnim <cloundyon31@gmail.com>
2024-11-24 02:20:02 +0900

    📝 DropdownPage 생성 및 코드 이동

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-
-function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-
-  return (
-    // ✨ Community Convention with Prop names
-    // selection -> value / onSelect -> onChange
-    <div className="flex">
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-    </div>
-  );
-}
+function App() {}
 
 export default App;
