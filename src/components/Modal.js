d2dc861495390115d1bd1d29f6ee84367c8f24e6
jiyoungnim <cloundyon31@gmail.com>
2024-11-26 23:08:17 +0900

    247. Modal Wrapup - body의 클래스에 overflow-hidden 추가, 모달과 오버레이의 포지션을 absolute -> fixed로 변경하여 버그 픽스

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,7 @@
+import { useEffect } from "react";
 import ReactDOM from "react-dom";
 
+// 🛠️ 242. Fixing the Modal with Portals
 // createPortal( first , second)
 // ReactDOM.createPortal은 모달과 같은 UI 요소를 ✨부모 컴포넌트와 별개로 다른 DOM 노드에 렌더링✨할 수 있게 합니다. 이 방식은 레이아웃이나 스타일 충돌을 피하고, 모달이 페이지 전체를 덮는 방식으로 렌더링되게 해 줍니다.
 
@@ -7,17 +9,29 @@ import ReactDOM from "react-dom";
 // 2️⃣ second: Modal 컴포넌트가 렌더링될 실제 DOM 위치 (여기서는 `.modal-container`) ->이 요소는 일반적으로 index.html 또는 App.js의 루트에서 미리 정의되어 있어야 합니다.
 // 👉 절대 positioned되지 않음 - 항상 position: static 유지 - Modal will be positioned relative to the HTML doc. - It'll always fill the entire screen!!
 function Modal({ onClose, children, actionBar }) {
+  // 247. Modal Wrapup
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
+
   return ReactDOM.createPortal(
     <div>
       {/* 오버레이 배경: 화면 전체를 덮고 어두운 배경을 만듬 */}
       <div
         onClick={onClose}
-        className="absolute inset-0 bg-gray-300 opacity-80"
+        className="fixed inset-0 bg-gray-300 opacity-80"
       ></div>
       {/* 실제 모달: 화면 중앙에 배치 */}
-      <div className="absolute inset-40 p-10 bg-white">
-        {children}
-        {actionBar}
+      {/* 🌈 absolute -> fixed: 어느 위치에서든 모달을 클릭하면 오버레이가 형성되도록, absolute로 하면 맨 위에서만 형성됨 */}
+      {/* absolute는 가장 가까운 위치의 지정된 조상요소(Position속성: relative, absolute or fixed)를 기준으로 배치되지만, fixed는 현재 화면(뷰포트)를 기준으로 배치되기 때문. 항상 화면 전체에 걸쳐 고정된 위치를 유지 */}
+      <div className="fixed inset-40 p-10 bg-white">
+        <div className="flex flex-col justify-between">
+          {children}
+          <div className="flex justify-end">{actionBar}</div>
+        </div>
       </div>
     </div>,
     document.querySelector(".modal-container")
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -8,11 +8,13 @@ function ModalPage() {
     setShowModal(true);
   };
 
+  // ⛔️ 243. Closing the Modal
   const handleClose = () => {
     // 호출될 때마다 모달이 닫히도록
     setShowModal(false);
   };
 
+  // 🌼 244. Customizing the Modal
   const actionBar = (
     <div>
       <Button onClick={handleClose} primary>
@@ -33,6 +35,150 @@ function ModalPage() {
         Open Modal
       </Button>
       {showModal && modal}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis
+        mauris eros, quis volutpat nulla interdum et. Aliquam quis lacinia
+        risus. Quisque in neque dictum, gravida erat pellentesque, finibus
+        nulla. Quisque fringilla, erat ut vestibulum ornare, risus justo
+        eleifend ipsum, iaculis ornare justo sapien eget nunc. Curabitur id odio
+        maximus, posuere nisi at, condimentum sem. Sed faucibus placerat ligula,
+        nec scelerisque nibh volutpat aliquam. Etiam pellentesque posuere odio,
+        quis tincidunt purus interdum eu. Donec venenatis odio sed fringilla
+        facilisis. Aenean vel condimentum mauris, eleifend condimentum leo.
+        Suspendisse at nulla odio. Nunc pellentesque ipsum at est dictum, non
+        vehicula orci molestie. Aliquam aliquet est non metus vulputate, a
+        tincidunt erat pellentesque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis
+        mauris eros, quis volutpat nulla interdum et. Aliquam quis lacinia
+        risus. Quisque in neque dictum, gravida erat pellentesque, finibus
+        nulla. Quisque fringilla, erat ut vestibulum ornare, risus justo
+        eleifend ipsum, iaculis ornare justo sapien eget nunc. Curabitur id odio
+        maximus, posuere nisi at, condimentum sem. Sed faucibus placerat ligula,
+        nec scelerisque nibh volutpat aliquam. Etiam pellentesque posuere odio,
+        quis tincidunt purus interdum eu. Donec venenatis odio sed fringilla
+        facilisis. Aenean vel condimentum mauris, eleifend condimentum leo.
+        Suspendisse at nulla odio. Nunc pellentesque ipsum at est dictum, non
+        vehicula orci molestie. Aliquam aliquet est non metus vulputate, a
+        tincidunt erat pellentesque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis
+        mauris eros, quis volutpat nulla interdum et. Aliquam quis lacinia
+        risus. Quisque in neque dictum, gravida erat pellentesque, finibus
+        nulla. Quisque fringilla, erat ut vestibulum ornare, risus justo
+        eleifend ipsum, iaculis ornare justo sapien eget nunc. Curabitur id odio
+        maximus, posuere nisi at, condimentum sem. Sed faucibus placerat ligula,
+        nec scelerisque nibh volutpat aliquam. Etiam pellentesque posuere odio,
+        quis tincidunt purus interdum eu. Donec venenatis odio sed fringilla
+        facilisis. Aenean vel condimentum mauris, eleifend condimentum leo.
+        Suspendisse at nulla odio. Nunc pellentesque ipsum at est dictum, non
+        vehicula orci molestie. Aliquam aliquet est non metus vulputate, a
+        tincidunt erat pellentesque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis
+        mauris eros, quis volutpat nulla interdum et. Aliquam quis lacinia
+        risus. Quisque in neque dictum, gravida erat pellentesque, finibus
+        nulla. Quisque fringilla, erat ut vestibulum ornare, risus justo
+        eleifend ipsum, iaculis ornare justo sapien eget nunc. Curabitur id odio
+        maximus, posuere nisi at, condimentum sem. Sed faucibus placerat ligula,
+        nec scelerisque nibh volutpat aliquam. Etiam pellentesque posuere odio,
+        quis tincidunt purus interdum eu. Donec venenatis odio sed fringilla
+        facilisis. Aenean vel condimentum mauris, eleifend condimentum leo.
+        Suspendisse at nulla odio. Nunc pellentesque ipsum at est dictum, non
+        vehicula orci molestie. Aliquam aliquet est non metus vulputate, a
+        tincidunt erat pellentesque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis
+        mauris eros, quis volutpat nulla interdum et. Aliquam quis lacinia
+        risus. Quisque in neque dictum, gravida erat pellentesque, finibus
+        nulla. Quisque fringilla, erat ut vestibulum ornare, risus justo
+        eleifend ipsum, iaculis ornare justo sapien eget nunc. Curabitur id odio
+        maximus, posuere nisi at, condimentum sem. Sed faucibus placerat ligula,
+        nec scelerisque nibh volutpat aliquam. Etiam pellentesque posuere odio,
+        quis tincidunt purus interdum eu. Donec venenatis odio sed fringilla
+        facilisis. Aenean vel condimentum mauris, eleifend condimentum leo.
+        Suspendisse at nulla odio. Nunc pellentesque ipsum at est dictum, non
+        vehicula orci molestie. Aliquam aliquet est non metus vulputate, a
+        tincidunt erat pellentesque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis
+        mauris eros, quis volutpat nulla interdum et. Aliquam quis lacinia
+        risus. Quisque in neque dictum, gravida erat pellentesque, finibus
+        nulla. Quisque fringilla, erat ut vestibulum ornare, risus justo
+        eleifend ipsum, iaculis ornare justo sapien eget nunc. Curabitur id odio
+        maximus, posuere nisi at, condimentum sem. Sed faucibus placerat ligula,
+        nec scelerisque nibh volutpat aliquam. Etiam pellentesque posuere odio,
+        quis tincidunt purus interdum eu. Donec venenatis odio sed fringilla
+        facilisis. Aenean vel condimentum mauris, eleifend condimentum leo.
+        Suspendisse at nulla odio. Nunc pellentesque ipsum at est dictum, non
+        vehicula orci molestie. Aliquam aliquet est non metus vulputate, a
+        tincidunt erat pellentesque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis
+        mauris eros, quis volutpat nulla interdum et. Aliquam quis lacinia
+        risus. Quisque in neque dictum, gravida erat pellentesque, finibus
+        nulla. Quisque fringilla, erat ut vestibulum ornare, risus justo
+        eleifend ipsum, iaculis ornare justo sapien eget nunc. Curabitur id odio
+        maximus, posuere nisi at, condimentum sem. Sed faucibus placerat ligula,
+        nec scelerisque nibh volutpat aliquam. Etiam pellentesque posuere odio,
+        quis tincidunt purus interdum eu. Donec venenatis odio sed fringilla
+        facilisis. Aenean vel condimentum mauris, eleifend condimentum leo.
+        Suspendisse at nulla odio. Nunc pellentesque ipsum at est dictum, non
+        vehicula orci molestie. Aliquam aliquet est non metus vulputate, a
+        tincidunt erat pellentesque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis
+        mauris eros, quis volutpat nulla interdum et. Aliquam quis lacinia
+        risus. Quisque in neque dictum, gravida erat pellentesque, finibus
+        nulla. Quisque fringilla, erat ut vestibulum ornare, risus justo
+        eleifend ipsum, iaculis ornare justo sapien eget nunc. Curabitur id odio
+        maximus, posuere nisi at, condimentum sem. Sed faucibus placerat ligula,
+        nec scelerisque nibh volutpat aliquam. Etiam pellentesque posuere odio,
+        quis tincidunt purus interdum eu. Donec venenatis odio sed fringilla
+        facilisis. Aenean vel condimentum mauris, eleifend condimentum leo.
+        Suspendisse at nulla odio. Nunc pellentesque ipsum at est dictum, non
+        vehicula orci molestie. Aliquam aliquet est non metus vulputate, a
+        tincidunt erat pellentesque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis
+        mauris eros, quis volutpat nulla interdum et. Aliquam quis lacinia
+        risus. Quisque in neque dictum, gravida erat pellentesque, finibus
+        nulla. Quisque fringilla, erat ut vestibulum ornare, risus justo
+        eleifend ipsum, iaculis ornare justo sapien eget nunc. Curabitur id odio
+        maximus, posuere nisi at, condimentum sem. Sed faucibus placerat ligula,
+        nec scelerisque nibh volutpat aliquam. Etiam pellentesque posuere odio,
+        quis tincidunt purus interdum eu. Donec venenatis odio sed fringilla
+        facilisis. Aenean vel condimentum mauris, eleifend condimentum leo.
+        Suspendisse at nulla odio. Nunc pellentesque ipsum at est dictum, non
+        vehicula orci molestie. Aliquam aliquet est non metus vulputate, a
+        tincidunt erat pellentesque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis
+        mauris eros, quis volutpat nulla interdum et. Aliquam quis lacinia
+        risus. Quisque in neque dictum, gravida erat pellentesque, finibus
+        nulla. Quisque fringilla, erat ut vestibulum ornare, risus justo
+        eleifend ipsum, iaculis ornare justo sapien eget nunc. Curabitur id odio
+        maximus, posuere nisi at, condimentum sem. Sed faucibus placerat ligula,
+        nec scelerisque nibh volutpat aliquam. Etiam pellentesque posuere odio,
+        quis tincidunt purus interdum eu. Donec venenatis odio sed fringilla
+        facilisis. Aenean vel condimentum mauris, eleifend condimentum leo.
+        Suspendisse at nulla odio. Nunc pellentesque ipsum at est dictum, non
+        vehicula orci molestie. Aliquam aliquet est non metus vulputate, a
+        tincidunt erat pellentesque.
+      </p>
+      {/* <Button onClick={handleClick} primary>
+        Open Modal
+      </Button>
+      {showModal && modal} */}
     </div>
   );
 }
